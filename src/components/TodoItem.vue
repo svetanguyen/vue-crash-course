7efd8576e7ffd992e9c3b31ef6f338b310850f54
svetanguyen <matematicaforeber@gmail.com>
2021-05-12 00:01:58 +0700

    change index of items

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -2,7 +2,7 @@
     <li>
         <span v-bind:class="{done: todo.completed}">
             <input type="checkbox" v-on:change="todo.completed = !todo.completed">
-            <strong>{{ todo.id }}</strong>
+            <strong>{{ index + 1 }}</strong>
             {{ todo.title }}
         </span>
         <button class="rm" v-on:click="$emit('remove-todo', todo.id)">&times;</button>
@@ -15,7 +15,8 @@ export default {
         todo: {
             type: Object,
             required: true
-        }
+        },
+        index: Number
     }    
 }
 </script>
